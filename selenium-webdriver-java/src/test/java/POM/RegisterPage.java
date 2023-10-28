e9204acd6ecce6bc63044efef18e43e2de2b0c3f
NGUYEN MINH THIEN PHUC <94725764+phuccoder@users.noreply.github.com>
2023-10-28 21:05:47 +0700

    Update

--- selenium-webdriver-java/src/test/java/POM/RegisterPage.java
+++ selenium-webdriver-java/src/test/java/POM/RegisterPage.java
@@ -9,38 +9,64 @@ public class RegisterPage {
 
     By myAccountLink = By.linkText("MY ACCOUNT");
     By createAccountLink = By.linkText("CREATE AN ACCOUNT");
-    By firstNameInputLocator = By.linkText("firstname");
-    By lastNameInputLocator = By.linkText("lastname");
-    By emailInputLocator = By.linkText("email_address");
+    By firstNameInputLocator = By.id("firstname");
+
+    By middleNameInputLocator = By.id("middlename");
+    By lastNameInputLocator = By.id("lastname");
+    By emailInputLocator = By.id("email_address");
     By passwordInputLocator = By.id("password");
     By confirmPasswordInputLocator = By.id("confirmation");
-    By registerButton = By.xpath("//button[@title='Register']");
+    By registerButton = By.cssSelector("button[title='Register']");
 
-    public RegisterPage(WebDriver driver){
-        this.driver =driver;
+    public RegisterPage(WebDriver driver) {
+        this.driver = driver;
     }
 
-    public void clickMyAccountLink(){
+    public void clickMyAccountLink() {
         driver.findElement(myAccountLink).click();
     }
+
     public void clickCreateAccountLink() {
         driver.findElement(createAccountLink).click();
     }
 
-    public void enterFirstName(String firstName){
+    public void enterFirstName(String firstName) {
         WebElement firstNameElement = driver.findElement(firstNameInputLocator);
         firstNameElement.clear();
         firstNameElement.sendKeys(firstName);
     }
 
-    public void enterLastName(String lastName){
+    public void enterMiddleName(String middleName){
+        WebElement middleNameElement = driver.findElement(middleNameInputLocator);
+        middleNameElement.clear();
+        middleNameElement.sendKeys(middleName);
+    }
+
+    public void enterLastName(String lastName) {
         WebElement lastNameElement = driver.findElement(lastNameInputLocator);
         lastNameElement.clear();
         lastNameElement.sendKeys(lastName);
     }
 
-    public void enterEmail (String email){
-
+    public void enterEmail(String email) {
+        WebElement emailElement = driver.findElement(emailInputLocator);
+        emailElement.clear();
+        emailElement.sendKeys(email);
     }
 
+    public void enterPassword(String password) {
+        WebElement passwordElement = driver.findElement(passwordInputLocator);
+        passwordElement.clear();
+        passwordElement.sendKeys(password);
+    }
+
+    public void enterConfirmPassword(String confirmPassword) {
+        WebElement confirmPasswordElement = driver.findElement(confirmPasswordInputLocator);
+        confirmPasswordElement.clear();
+        confirmPasswordElement.sendKeys(confirmPassword);
+    }
+
+    public void clickRegister() {
+        driver.findElement(registerButton).click();
+    }
 }
